1608e6fd0c575ac2d80e57b5ec8a6aeed76a7d22
javadbat <javadbat@gmail.com>
2024-12-01 19:23:31 +0330

    fix show error

--- react/lib/JBForm.tsx
+++ react/lib/JBForm.tsx
@@ -1,11 +1,13 @@
 import React, { useCallback, useImperativeHandle } from 'react';
-import {JBFormWebComponent} from 'jb-form';
+import {type JBFormWebComponent} from 'jb-form';
 import { useBindEvent } from '../../../../common/hooks/use-event.js';
 type Props = React.HTMLProps<HTMLFormElement> & {
- onSubmit:(e:SubmitEvent)=>void
+ onSubmit?:(e:SubmitEvent)=>void
+ onDirtyChange?:(e:CustomEvent)=>void
+ onValidityChange?:(e:CustomEvent)=>void
 }
 export const JBForm = React.forwardRef((props:Props, ref:React.ForwardedRef<JBFormWebComponent|undefined>)=> {
-  const {onSubmit, children, ...formProps} = props;
+  const {onSubmit,onValidityChange,onDirtyChange, children, ...formProps} = props;
   const element = React.useRef<JBFormWebComponent>(null);
   useImperativeHandle(
     ref,
@@ -17,9 +19,21 @@ export const JBForm = React.forwardRef((props:Props, ref:React.ForwardedRef<JBFo
       onSubmit(e);
     }
   },[onSubmit]);
+  const onValidityChangeFunc = useCallback((e:CustomEvent)=>{
+    if(typeof onValidityChange == "function"){
+      onValidityChangeFunc(e);
+    }
+  },[onSubmit]);
+  const onDirtyChangeFunc = useCallback((e:CustomEvent)=>{
+    if(typeof onDirtyChange == "function"){
+      onDirtyChange(e);
+    }
+  },[onSubmit]);
   useBindEvent(element,"submit",onSubmitFunc);
+  useBindEvent(element,"dirty-change",onDirtyChangeFunc);
+  useBindEvent(element,"validity-change",onValidityChangeFunc);
   return (
-    <form is="jb-form" {...formProps}>
+    <form ref={element} is="jb-form" {...formProps}>
       {
         children
       }
