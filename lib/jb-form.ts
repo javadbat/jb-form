93fd900a6177a2a8d0c05d6f2d0da8a8db3972cc
javadbat <javadbat@gmail.com>
2024-12-05 13:24:57 +0330

    add type export

--- lib/jb-form.ts
+++ lib/jb-form.ts
@@ -4,6 +4,7 @@ import { VirtualElement } from './virtual-element';
 import { VirtualElementList } from './virtual-element-list';
 import { SubFormList } from './sub-form-list';
 export * from './types';
+export {VirtualElement};
 export class JBFormWebComponent extends HTMLFormElement {
   //keep original form check validity
   #formCheckValidity = this.checkValidity;
